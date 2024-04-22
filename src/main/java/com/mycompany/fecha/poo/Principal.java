e95ee47db3edfed882690d69e19d8fc56ed18b32
Alejandra Durán <79487144+Alejandraqt@users.noreply.github.com>
2024-04-21 20:24:10 -0500

    Actualización método principal

--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,25 @@
 package com.mycompany.fecha.poo;
 
+import java.util.Scanner;
+
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+    /**
+     * @param args
+     *
+     * Complejidad temporal: O(1) Tiempo Constante
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int diaRecibido = sc.nextInt();
+        int mesRecibido = sc.nextInt();
+        int anioRecibido = sc.nextInt();
+        
+        Fecha miFecha = new Fecha(diaRecibido, mesRecibido, anioRecibido);
+        
+        if(miFecha.validarFecha()){
+            System.out.println("La fecha es válida");
+            System.out.println(miFecha.fechaLarga());
+        }
+    }
 }
